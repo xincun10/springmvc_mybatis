6c7da82f29fed42a1fac93a74d4b8bb3ecb82997
xincun10 <zhengchongyang@bupt.edu.cn>
2018-09-10 16:10:29 +0800

    restful支持
    1.restful
    2.静态资源解析

--- src/com/zcy/ssm/controller/ItemsController.java
+++ src/com/zcy/ssm/controller/ItemsController.java
@@ -13,9 +13,11 @@ import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.multipart.MultipartFile;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -180,5 +182,17 @@ public class ItemsController {
 	public String editItemsAllSubmit(ItemsQueryVo itemsQueryVo) throws Exception
 	{
 		return "success";
+	}
+	
+	//使用RESTful格式查询商品信息
+	///itemsView/{id}里面的{id}表示将这个位置的参数传到@PathVariable指定的名称中
+	//例如itemsView/{id}/{type},参数可以写成(@PathVariable("id") Integer id,
+	//@PathVariable("type") String abc)
+	@RequestMapping("/itemsView/{id}")
+	public @ResponseBody ItemsCustom itemsView(@PathVariable("id") Integer id)
+			throws Exception
+	{
+		ItemsCustom itemsCustom = itemsService.findItemsById(id);
+		return itemsCustom;
 	}
 }
